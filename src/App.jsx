d5c8e34fff60ea68b562defb69affa36489aa876
Prathmesh Ojha <67790692+ojhaprathmesh@users.noreply.github.com>
2025-04-19 18:41:41 +0530

    Revert "Bit-By-Bit page completed!"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import { Navigate } from "react-router-dom";
 import Homepage from "./Pages/HomePage";
-import BitByBit from "./Pages/BitbyBit";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Loader from "./Loader/Loader.jsx";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [Loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -16,17 +15,18 @@ function App() {
 
   return (
     <Router>
-      {loading ? (
-        <Loader />
-      ) : (
+      
+   
         <Routes>
           <Route element={<Homepage />} path="/" />
-          <Route element={<Navigate to="/bit-by-bit" replace />} path="/clue-hunt" />
-          <Route element={<BitByBit />} path="/bit-by-bit" />
+          
+
         </Routes>
-      )}
+    
     </Router>
   );
 }
 
 export default App;
+
+/*hi*/
--- src/navbar/navbar1.jsx
+++ src/navbar/navbar1.jsx
@@ -10,7 +10,7 @@ const CosmicNav = () => {
 
   const navItems = [
     { id: 'home', label: 'Home' },
-    { id: 'aboutglitch', label: 'About' },
+    { id: 'about', label: 'About' },
     { id: 'memories', label: 'Memories' },
     { id: 'sponsors', label: 'Sponsors' },
     { id: 'timeline', label: 'Timeline' },
